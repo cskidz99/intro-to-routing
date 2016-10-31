afdc81e2e5daf5ff27744be9097dbc585f01474f
Cameron Skidmore <cskidz99@Camerons-MacBook-Air.local>
2016-10-31 11:46:01 -0600

    First practice with Jeremy

--- app/DetailsController.js
+++ app/DetailsController.js
@@ -1,3 +1,9 @@
-app.controller('DetailsController', function(){
+app.controller('DetailsController', function($scope,$stateParams,DataService){
+
+  $scope.currentDetailId = $stateParams.detailId;
+
+  $scope.currentThing = DataService.data
+  [$stateParams.detailId];
+
 
 });
